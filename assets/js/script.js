60dc6b9f9e224bc8e01270a052c49008519d4bb1
Mady0869 <madhavi.neves@gmail.com>
2023-02-14 15:54:05 -0300

    Ajuste navbar início hero
    Navbar ajustada, agora fica transparente e segue a página. Hero agora possui tamanho e bg image

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@ const bar = document.getElementById('bar')
 const close = document.getElementById('close')
 const nav = document.getElementById('nav')
 
+const navbar = document.getElementById('navbar')
+var sticky = navbar.offsetTop
+
+window.onscroll = function() {myFunction()}
+
+
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active')
@@ -13,3 +19,11 @@ if (close) {
         nav.classList.remove('active')
     })
 }
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+      navbar.classList.add("top");
+    } else {
+      navbar.classList.remove("top");
+    }
+  }
