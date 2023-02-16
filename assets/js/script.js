e16c539064e1f0f15e64d763121a87c71e6942b1
LucasUriel2002 <lucas.uriel2002@gmail.com>
2023-02-16 17:07:13 -0300

    E-mail
    Adicionado formulario de contato por E-mail

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,41 +28,4 @@ function myFunction() {
     }
   }
 
-  //scroler 
-
-  "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"
-  const swiper = new Swiper('.swiper', {
-      // Optional parameters
-      slidesPerView: 1,
-      spaceBetween: 20,
-      loop: true,
-      
-    
-      breakpoints: {
-          // when window width is >= 320px
-          428: {
-            slidesPerView: 3,
-            spaceBetween: 20
-          }
-          // when window width is >= 640px 
-      },
   
-  
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-  
-        
-      },
-    });
